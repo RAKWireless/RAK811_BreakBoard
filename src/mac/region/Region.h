e618a100b8c883143beea498d4104c2976741c79
RAKwireless <frank.lu@rakwireless.com>
2018-07-17 11:05:32 +0800

    Update macro definition

--- src/mac/region/Region.h
+++ src/mac/region/Region.h
@@ -52,8 +52,12 @@
 #ifndef __REGION_H__
 #define __REGION_H__
 
-
-
+#define REGION_US915
+#define REGION_AU915
+#define REGION_AS923
+#define REGION_KR920
+#define REGION_EU868
+#define REGION_IN865
 
 /*!
  * Macro to compute bit of a channel index.
